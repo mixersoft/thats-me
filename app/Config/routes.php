ce8ca93b3583cdbb64bc4f29c41668757fc46c2d
michael <michael@snaphappi.com>
2012-12-23 07:19:28 -0500

    routes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -26,6 +26,7 @@
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
  Router::connect('/', array('controller' => 'thatsme', 'action' => 'launchsnappi'));
+ Router::connect('/:action', array('controller' => 'thatsme'));
 	// Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'launchrock'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
